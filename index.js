bf6b615977b88fb438b6051b76e27c321b39cc5b
Aruna <aruna.evan15@gmail.com>
2021-09-11 01:53:25 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.getElementById("main").remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Aruna is the champion"
